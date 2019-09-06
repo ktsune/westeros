eea01dc8df9a256817105527bd99341dd65347b0
Sarah Tatro <statrox4@gmail.com>
2019-09-06 09:51:19 -0600

    Setup gems and create test for user searching members of specific GoT house

---
+++ spec/features/user_can_select_members_from_dropdown_spec.rb
@@ -0,0 +1,27 @@
+# As a user
+# When I visit "/"
+# And I select "Greyjoy" from the dropdown
+# And I click on "Get Members"
+# Then my path should be "/search" with "house=greyjoy" in the parameters
+# And I should see a message "7 Members"
+# And I should see a list of the 7 members of House Greyjoy
+# And I should see a name and id for each member.
+
+require 'rails_helper'
+
+feature 'westeros api' do
+  scenario 'user can get members from a GoT House' do
+    visit '/'
+
+    select "Greyjoy"
+    click_on "Get Members"
+
+    expect(current_path).to eq("/search")
+    expect(page).to have_content("7 members")
+
+    within(first('.member')) do
+      expect(page).to have_content('.name')
+      expect(page).to have_content('.id')
+    end
+  end
+end 
